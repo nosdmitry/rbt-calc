00b04db99a43aca59343bff98fef94c4d3e93775
Dmitry Nosov <mizai@yandex.ru>
2021-04-15 10:23:02 +0300

    Refactor: constants file

--- src/index.js
+++ src/index.js
@@ -1,73 +1,10 @@
 import './index.scss';
-// import RangeInput from './components/RangeInput';
-// import TextInput from './components/TextInput';
-// import Result from './components/Result';
 
-// const allData = {
-//   '#flat-square-range': 0,
-//   '#flat-restroom-square': 0,
-// }
-
-// const flSquareRange = new RangeInput(10, 200, 30, '#flat-square-range', result);
-// flSquareRange.setCurrentValue();
-// flSquareRange.setEventListenters();
-
-// const restRoomSquareRange = new RangeInput(2, 20, 5, '#flat-restroom-square', result);
-// restRoomSquareRange.setCurrentValue();
-// restRoomSquareRange.setEventListenters();
-
-// const result = new Result({
-//   data: allData,
-//   setData: () => {
-//     const flatSq = flSquareRange.setCurrentValue();
-//     return {
-//       '#flat-square-range': flatSq['#flat-square-range'],
-//     }
-//   }
-// }, '.calculate__footer-result-common_type_number');
-
-// result.showData();
-
-
-// const flSquareInput = new TextInput(10, 200, flSquareRange.getInputValues(), '#flat-square-input');
-// flSquareInput.setEventListenters();
-
-// action-block__range
-
-// document.querySelector('.myinput').oninput = function() {
-//   var value = (this.value-this.min)/(this.max-this.min)*100
-//   this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
-// };
-
-const flSquareRange = document.querySelector('#flat-square-range');
-const flSquareInput = document.querySelector('#flat-square-input');
-const flatRestRoom = document.querySelector('#flat-restroom');
-const flatRestRoomSquareRange = document.querySelector('#flat-restroom-square-range');
-const flatRestRoomSquareInput = document.querySelector('#flat-restroom-square-input');
-const flatType = document.querySelector('#flat-type');
-const flatElectricity = document.querySelector('#flat-electricity');
-const constructionType = document.querySelector('#flat-constr-type');
-const flatCeilings = document.querySelector('#flat-ceiling');
-const flatFloor = document.querySelector('#flat-floor');
-const flatDoors = document.querySelector('#flat-doors');
-const flatEntranceDoor = document.querySelector('#flat-entrance-door');
-const flatEntranceDoorConfirm = document.querySelector('.action-block__switch_type_confirm');
-
-const total = document.querySelector('.calculate__footer-result-common_type_number');
-
-
-/* menu */
-const menuButton = document.querySelector('.header__button_type_menu');
-const menuCloseButton = document.querySelector('.menu__close-button');
-const menuContainer = document.querySelector('.menu');
-
-const searchButton = document.querySelector('.header__button_type_search');
-const searchMobileForm = document.querySelector('.header__search-form');
-const searchFormClose = document.querySelector('.header__search-form-close');
-
-
-/* menu */
-
+import { menuButton, menuCloseButton, menuContainer, searchButton, 
+  searchMobileForm, searchFormClose, flSquareRange, flSquareInput, 
+  flatRestRoom, flatRestRoomSquareRange, flatRestRoomSquareInput, flatType, flatElectricity, 
+  constructionType, flatCeilings, flatFloor, flatDoors, flatEntranceDoor, flatEntranceDoorConfirm, total
+} from './utils/constants';
 
 
 function totalCount() {  
@@ -96,24 +33,21 @@ function numberWithSpaces(x) {
 }
 
 
+showResult();
+
 
 searchFormClose.addEventListener('click', () => {
   searchMobileForm.classList.remove('header__search-form_type_visible');
 });
-
 searchButton.addEventListener('click', () => {
   searchMobileForm.classList.add('header__search-form_type_visible');
-})
-
+});
 menuCloseButton.addEventListener('click', () => {
   menuContainer.classList.add('menu_type_hidden');
-})
-
+});
 menuButton.addEventListener('click', () => {
   menuContainer.classList.toggle('menu_type_hidden');
 });
-
-
 
 flatRestRoom.addEventListener('input', showResult);
 flatType.addEventListener('input', showResult);
@@ -148,7 +82,3 @@ flatRestRoomSquareRange.addEventListener('input', (evt) => {
   showResult();
   flatRestRoomSquareInput.value = evt.target.value;
 });
-
-
-
-showResult();
---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+/*** header buttons ***/
+export const menuButton = document.querySelector('.header__button_type_menu');
+export const menuCloseButton = document.querySelector('.menu__close-button');
+export const menuContainer = document.querySelector('.menu');
+
+export const searchButton = document.querySelector('.header__button_type_search');
+export const searchMobileForm = document.querySelector('.header__search-form');
+export const searchFormClose = document.querySelector('.header__search-form-close');
+/*** header buttons ***/
+
+/*** calculation logic ***/
+export const flSquareRange = document.querySelector('#flat-square-range');
+export const flSquareInput = document.querySelector('#flat-square-input');
+export const flatRestRoom = document.querySelector('#flat-restroom');
+export const flatRestRoomSquareRange = document.querySelector('#flat-restroom-square-range');
+export const flatRestRoomSquareInput = document.querySelector('#flat-restroom-square-input');
+export const flatType = document.querySelector('#flat-type');
+export const flatElectricity = document.querySelector('#flat-electricity');
+export const constructionType = document.querySelector('#flat-constr-type');
+export const flatCeilings = document.querySelector('#flat-ceiling');
+export const flatFloor = document.querySelector('#flat-floor');
+export const flatDoors = document.querySelector('#flat-doors');
+export const flatEntranceDoor = document.querySelector('#flat-entrance-door');
+export const flatEntranceDoorConfirm = document.querySelector('.action-block__switch_type_confirm');
+
+export const total = document.querySelector('.calculate__footer-result-common_type_number');
+/*** calculation logic ***/
+
